a91b8dc4ae99b8c928c7cd858353bd79c96d17e3
sbm367 <sbm367@protonmail.com>
2021-02-17 20:26:23 -0500

    added sockets

---
+++ sockets.py
@@ -0,0 +1,14 @@
+import socket
+
+# Create Socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET is ipv4 
+# SOCK_STREAM is TCP
+# So this is a tcp/ip connection
+
+#Server code
+s.bind( (socket.gethostname(), 1234) )
+
+#queue
+s.listen(5)
