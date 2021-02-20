856eaf9ae89a22d27f24a8ba05b8cab2132f1895
sbm367 <sbm367@protonmail.com>
2021-02-19 22:43:08 -0500

    changed url

--- urllib_test.py
+++ urllib_test.py
@@ -1,5 +1,8 @@
 #Test of urlib, specificly the requests module
 import urllib.request
 
-x = urllib.request.urlopen('https://www.google.com/')
+site = 'https://www.york.ac.uk/teaching/cws/wws/webpage1.html'
+
+#x = urllib.request.urlopen('https://www.google.com/')
+
 print(x.read())
