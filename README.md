980c522ba85ea95245052d35289bbfb99f40c327
repl.it user <replituser@example.com>
2021-02-22 23:58:26 +0000

    added bs4 ex, updatred readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #Python Networking Project
 
-The purpose of this project is to  learn the urlib and sockets libs in Python
+The purpose of this project is to learn basic netwokring in Python
+
+The technologies covered in these examples are:
+
+* TCP/IP Model
+* Sockets
+* urllib
+* BeautifulSoup
+
--- urllib_short.py
+++ urllib_short.py
@@ -2,5 +2,13 @@ import urllib.request, urllib.parse, urllib.error
 
 url = 'http://data.pr4e.org/romeo.txt'
 fhand = urllib.request.urlopen(url)
+#optional param in url open to preserve headers
 for line in fhand:
   print ( line.decode().strip() )
+
+  counts = dict()
+  for line in fhand:
+    words = line.decode().split()
+    for word in words:
+      counts[word] = counts.get(word,0)+1
+print(counts)
