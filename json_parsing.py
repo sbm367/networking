7b658b4a737eb41f87d8ab496ffc5eb825d07d40
sbm367 <sbm367@protonmail.com>
2021-02-24 19:08:13 -0500

    extended json parsing example

--- json_parsing.py
+++ json_parsing.py
@@ -1,5 +1,6 @@
 import json
 
+# dictionary example
 data = '''
 {
     "name" : "Chuck",
@@ -12,7 +13,32 @@ data = '''
     }
 }
 '''
-
+# loads vs load means load from string
 info = json.loads(data)
 print('Name : ', info['name'])
 print('Hide? : ', info['email']['hide'])
+
+# list example
+data2 = '''
+[
+   {
+       "id" : "001",
+       "x" : "2",
+       "name" : "Chuck"
+   },
+   {
+       "id" : "009",
+       "x" : "7",
+       "name" : "Charles"
+   }
+]
+'''
+
+info2 = json.loads(data2)
+print('User count : ', len(info2))
+
+
+for user in info2:
+    print("\n User data")
+    for key in user.keys():
+        print(key, ":", user[key])
